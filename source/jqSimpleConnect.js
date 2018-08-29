a6ca3608921a905989ed598fe2be367b87fd34ed
Paulo Vitor de Lima <paulo.lima@svisolucoes.com.br>
2018-08-29 11:53:13 -0300

    Added Show/Hide support and repaintAll to window.resize event.

--- source/jqSimpleConnect.js
+++ source/jqSimpleConnect.js
@@ -24,15 +24,15 @@ jqSimpleConnect._connections = new Object();
  * 
  * @param {object} connection A connection object.
  */
-jqSimpleConnect._positionConnection = function(connection) {
+jqSimpleConnect._positionConnection = function (connection) {
     // Calculate the positions of the element's center.
     var posA = connection.elementA.offset();
-    posA.left = parseInt(posA.left, 10) + parseInt(connection.elementA.outerWidth()/2, 10);
-    posA.top = parseInt(posA.top, 10) + parseInt(connection.elementA.outerHeight()/2, 10);
+    posA.left = parseInt(posA.left, 10) + parseInt(connection.elementA.outerWidth() / 2, 10);
+    posA.top = parseInt(posA.top, 10) + parseInt(connection.elementA.outerHeight() / 2, 10);
 
     var posB = connection.elementB.offset();
-    posB.left = parseInt(posB.left, 10) + parseInt(connection.elementB.outerWidth()/2, 10);
-    posB.top = parseInt(posB.top, 10) + parseInt(connection.elementB.outerHeight()/2, 10);
+    posB.left = parseInt(posB.left, 10) + parseInt(connection.elementB.outerWidth() / 2, 10);
+    posB.top = parseInt(posB.top, 10) + parseInt(connection.elementB.outerHeight() / 2, 10);
 
     // Get the line's elements.
     var line1 = jQuery('#' + connection.id + '_1');
@@ -40,14 +40,14 @@ jqSimpleConnect._positionConnection = function(connection) {
     var line3 = jQuery('#' + connection.id + '_3');
 
     // Verify if the elements are aligned in a horizontal or vertical line.
-    if(posA.left == posB.left || posA.top == posB.top) {
+    if (posA.left == posB.left || posA.top == posB.top) {
         // Uses only one line (hide the other two).
         line1.show();
         line2.hide();
         line3.hide();
 
         // Verify if the line must be vertical or horizonal.
-        if(posA.left == posB.left) {
+        if (posA.left == posB.left) {
             // Vertical line.
             jqSimpleConnect._positionVerticalLine(line1, posA, posB, connection.radius, connection.roundedCorners);
         } else {
@@ -56,19 +56,19 @@ jqSimpleConnect._positionConnection = function(connection) {
         }
     } else {
         // Verify if must use two lines or three.
-        if(connection.anchorA != connection.anchorB) {
+        if (connection.anchorA != connection.anchorB) {
             // Use two lines (hide the third).
             line1.show();
             line2.show();
             line3.hide();
- 
+
             // Check the anchors of the elements.
             var corner = new Object();
-            if(connection.anchorA == 'vertical') {
+            if (connection.anchorA == 'vertical') {
                 // Find the corner's position.
                 corner.left = posA.left;
                 corner.top = posB.top;
-                
+
                 // Draw lines.
                 jqSimpleConnect._positionVerticalLine(line1, posA, corner, connection.radius, connection.roundedCorners);
                 jqSimpleConnect._positionHorizontalLine(line2, posB, corner, connection.radius, connection.roundedCorners);
@@ -76,7 +76,7 @@ jqSimpleConnect._positionConnection = function(connection) {
                 // Find the corner's position.
                 corner.left = posB.left;
                 corner.top = posA.top;
-                
+
                 // Draw lines.
                 jqSimpleConnect._positionVerticalLine(line1, posB, corner, connection.radius, connection.roundedCorners);
                 jqSimpleConnect._positionHorizontalLine(line2, posA, corner, connection.radius, connection.roundedCorners);
@@ -86,30 +86,30 @@ jqSimpleConnect._positionConnection = function(connection) {
             line1.show();
             line2.show();
             line3.show();
-            
+
             // Declare connection points.
             var corner1 = new Object();
             var corner2 = new Object();
-            
+
             // Find if the middle's line must be vertical o horizontal.
-            if(connection.anchorA == 'vertical') {
+            if (connection.anchorA == 'vertical') {
                 // Middle's line must be horizontal.
-                corner1.top = parseInt((posA.top + posB.top)/2, 10);
+                corner1.top = parseInt((posA.top + posB.top) / 2, 10);
                 corner2.top = corner1.top;
                 corner1.left = posA.left;
                 corner2.left = posB.left;
-                
+
                 // Draw lines.
                 jqSimpleConnect._positionVerticalLine(line1, posA, corner1, connection.radius, connection.roundedCorners);
                 jqSimpleConnect._positionVerticalLine(line2, posB, corner2, connection.radius, connection.roundedCorners);
                 jqSimpleConnect._positionHorizontalLine(line3, corner1, corner2, connection.radius, connection.roundedCorners);
             } else {
                 // Middle's line must be vertical.
-                corner1.left = parseInt((posA.left + posB.left)/2, 10);
+                corner1.left = parseInt((posA.left + posB.left) / 2, 10);
                 corner2.left = corner1.left;
                 corner1.top = posA.top;
                 corner2.top = posB.top;
-                
+
                 // Draw lines.
                 jqSimpleConnect._positionHorizontalLine(line1, posA, corner1, connection.radius, connection.roundedCorners);
                 jqSimpleConnect._positionHorizontalLine(line2, posB, corner2, connection.radius, connection.roundedCorners);
@@ -128,12 +128,12 @@ jqSimpleConnect._positionConnection = function(connection) {
  *@param {integer} radius The line's radius.
  *@param {boolean} roundedCorners A boolean indicating if the corners are going to be round.
  */
-jqSimpleConnect._positionVerticalLine = function(jqElement, point1, point2, radius, roundedCorners) {
-    var halfRadius = parseInt(radius/2, 10);
+jqSimpleConnect._positionVerticalLine = function (jqElement, point1, point2, radius, roundedCorners) {
+    var halfRadius = parseInt(radius / 2, 10);
     jqElement.css('left', point1.left - halfRadius);
-    jqElement.css('top', ((point1.top > point2.top)? (point2.top - halfRadius) : (point1.top - halfRadius)));
+    jqElement.css('top', ((point1.top > point2.top) ? (point2.top - halfRadius) : (point1.top - halfRadius)));
     jqElement.css('width', radius + 'px');
-    jqElement.css('height', ((point1.top > point2.top)? (point1.top - point2.top + radius) : (point2.top - point1.top + radius) ) + 'px');
+    jqElement.css('height', ((point1.top > point2.top) ? (point1.top - point2.top + radius) : (point2.top - point1.top + radius)) + 'px');
 }
 
 /**
@@ -145,12 +145,12 @@ jqSimpleConnect._positionVerticalLine = function(jqElement, point1, point2, radi
  *@param {integer} radius The line's radius.
  *@param {boolean} roundedCorners A boolean indicating if the corners are going to be round.
  */
-jqSimpleConnect._positionHorizontalLine = function(jqElement, point1, point2, radius, roundedCorners) {
-    var halfRadius = parseInt(radius/2, 10);
+jqSimpleConnect._positionHorizontalLine = function (jqElement, point1, point2, radius, roundedCorners) {
+    var halfRadius = parseInt(radius / 2, 10);
     jqElement.css('top', point1.top - halfRadius);
-    jqElement.css('left', ((point1.left > point2.left)? (point2.left - halfRadius) : (point1.left - halfRadius)));
+    jqElement.css('left', ((point1.left > point2.left) ? (point2.left - halfRadius) : (point1.left - halfRadius)));
     jqElement.css('height', radius + 'px');
-    jqElement.css('width', ((point1.left > point2.left)? (point1.left - point2.left + radius) : (point2.left - point1.left + radius) ) + 'px');
+    jqElement.css('width', ((point1.left > point2.left) ? (point1.left - point2.left + radius) : (point2.left - point1.left + radius)) + 'px');
 }
 
 /**
@@ -163,46 +163,56 @@ jqSimpleConnect._positionHorizontalLine = function(jqElement, point1, point2, ra
  * 'horizontal' or 'vertical') and 'anchorB' (the anchor type of second element).
  * @returns {string} The connection identifier or 'null' if the connection could not be draw.
  */
-jqSimpleConnect.connect = function(elementA, elementB, options) {
+jqSimpleConnect.connect = function (elementA, elementB, options) {
     // Verify if the element's selector are ok.
-    if(elementA == null || jQuery(elementA).size() == 0 ||
-       elementB == null || jQuery(elementB).size() == 0) {
-       return null;
-       }
+    if (elementA == null || jQuery(elementA).size() == 0 ||
+        elementB == null || jQuery(elementB).size() == 0) {
+        return null;
+    }
 
     elementA = jQuery(elementA);
-    if(elementA.size() > 1) elementA = elementA.first();
+    if (elementA.size() > 1) elementA = elementA.first();
     elementB = jQuery(elementB);
-    if(elementB.size() > 1) elementB = elementB.first();
+    if (elementB.size() > 1) elementB = elementB.first();
 
     // Create connection object.
     var connection = new Object();
     connection.id = 'jqSimpleConnect_' + jqSimpleConnect._idGenerator++;
     connection.elementA = elementA;
     connection.elementB = elementB;
-    connection.color = (options != null && options.color != null)? options.color + '' : '#808080';
-    connection.radius = (options != null && options.radius != null && !isNaN(options.radius))? parseInt(options.radius, 10) : 5;
-    connection.anchorA = (options != null && options.anchorA != null && (options.anchorA == 'vertical' || options.anchorA == 'horizontal'))? options.anchorA : 'horizontal';
-    connection.anchorB = (options != null && options.anchorB != null && (options.anchorB == 'vertical' || options.anchorB == 'horizontal'))? options.anchorB : 'horizontal';
+    connection.color = (options != null && options.color != null) ? options.color + '' : '#808080';
+    connection.radius = (options != null && options.radius != null && !isNaN(options.radius)) ? parseInt(options.radius, 10) : 5;
+    connection.anchorA = (options != null && options.anchorA != null && (options.anchorA == 'vertical' || options.anchorA == 'horizontal')) ? options.anchorA : 'horizontal';
+    connection.anchorB = (options != null && options.anchorB != null && (options.anchorB == 'vertical' || options.anchorB == 'horizontal')) ? options.anchorB : 'horizontal';
     connection.roundedCorners = options != null && options.roundedCorners != null && (options.roundedCorners == true || options.roundedCorners == 'true');
 
     // Add connection to the connection's list.
     jqSimpleConnect._connections[connection.id] = connection;
 
     // Create HTML elements.
-    var div = '<div id="divUniqueIdentifier" class="jqSimpleConnect '+connection.id+'" ' + 
-              'style="width:'+connection.radius+'px; ' +
-                     'height:'+connection.radius+'px; ' +
-                     'background-color:'+connection.color+'; ' +
-                     (connection.roundedCorners? 'border-radius:'+parseInt(connection.radius/2,10)+'px; -webkit-border-radius:'+parseInt(connection.radius/2,10)+'px; -moz-border-radius:'+parseInt(connection.radius/2,10)+'px; ' : '') + 
-                     'position:absolute;"></div>';
-    jQuery('body').prepend(div.replace('divUniqueIdentifier', connection.id + '_1'));
-    jQuery('body').prepend(div.replace('divUniqueIdentifier', connection.id + '_2'));
-    jQuery('body').prepend(div.replace('divUniqueIdentifier', connection.id + '_3'));
+    var div = '<div id="divUniqueIdentifier" class="jqSimpleConnect ' + connection.id + '" ' +
+        'style="width:' + connection.radius + 'px; ' +
+        'height:' + connection.radius + 'px; ' +
+        'background-color:' + connection.color + '; ' +
+        (connection.roundedCorners ? 'border-radius:' + parseInt(connection.radius / 2, 10) + 'px; -webkit-border-radius:' + parseInt(connection.radius / 2, 10) + 'px; -moz-border-radius:' + parseInt(connection.radius / 2, 10) + 'px; ' : '') +
+        'position:absolute;"></div>',
+        div1, div2, div3;
+
+    div1 = jQuery(div.replace('divUniqueIdentifier', connection.id + '_1'));
+    div2 = jQuery(div.replace('divUniqueIdentifier', connection.id + '_2'));
+    div3 = jQuery(div.replace('divUniqueIdentifier', connection.id + '_3'));
+    if(options !== null && typeof options.onClickRemove !== 'undefined'){
+        div1.on('click',options.onClickRemove);
+        div2.on('click',options.onClickRemove);
+        div3.on('click',options.onClickRemove);
+    }
+    jQuery('body').prepend(div1);
+    jQuery('body').prepend(div2);
+    jQuery('body').prepend(div3);
 
     // Position connection.
     jqSimpleConnect._positionConnection(connection);
-    
+
     // Return result.
     return connection.id;
 }
@@ -213,9 +223,9 @@ jqSimpleConnect.connect = function(elementA, elementB, options) {
  * @param {string} connectionId The connection identifier.
  * @returns {boolean} 'true' if the operation was done, 'false' if the connection no exists.
  */
-jqSimpleConnect.repaintConnection = function(connectionId) {
+jqSimpleConnect.repaintConnection = function (connectionId) {
     var connection = jqSimpleConnect._connections[connectionId];
-    if(connection != null) {
+    if (connection != null) {
         jqSimpleConnect._positionConnection(connection);
         return true;
     }
@@ -225,8 +235,8 @@ jqSimpleConnect.repaintConnection = function(connectionId) {
 /**
  * Repaints all the connections.
  */
-jqSimpleConnect.repaintAll = function() {
-    for(var key in jqSimpleConnect._connections) {
+jqSimpleConnect.repaintAll = function () {
+    for (var key in jqSimpleConnect._connections) {
         jqSimpleConnect._positionConnection(jqSimpleConnect._connections[key]);
     }
 }
@@ -237,31 +247,100 @@ jqSimpleConnect.repaintAll = function() {
  * @param {string} connectionId The connection identifier.
  * @returns {boolean} 'true' if the operation was done, 'false' if the connection no exists.
  */
-jqSimpleConnect.removeConnection = function(connectionId) {
-    if(jqSimpleConnect._connections[connectionId] != null) {
+jqSimpleConnect.removeConnection = function (connectionId) {
+    if (jqSimpleConnect._connections[connectionId] != null) {
         // Remove HTML element.
-        jQuery('.jqSimpleConnect.'+connectionId).remove();
-        
+        jQuery('.jqSimpleConnect.' + connectionId).remove();
+
         // Remove connection data.
         jqSimpleConnect._connections[connectionId] = null;
         delete jqSimpleConnect._connections[connectionId];
-        
+
         // Return result.
         return true;
     }
     return false;
 }
 
+
+/**
+ * Removes a connection.
+ *
+ * @param {string} connectionId The connection identifier.
+ * @returns {boolean} 'true' if the operation was done, 'false' if the connection no exists.
+ */
+jqSimpleConnect.hideConnection = function (connectionId) {
+    if (jqSimpleConnect._connections[connectionId] != null) {
+        // Remove HTML element.
+        jQuery('.jqSimpleConnect.' + connectionId).hide();
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Removes a connection.
+ *
+ * @param {string} connectionId The connection identifier.
+ * @returns {boolean} 'true' if the operation was done, 'false' if the connection no exists.
+ */
+jqSimpleConnect.hideConnections = function (connectionId) {
+    for (var connId in connectionId) {
+        jqSimpleConnect.hideConnection(connectionId[connId]);
+    }
+}
+
+
+/**
+ * Removes a connection.
+ *
+ * @param {string} connectionId The connection identifier.
+ * @returns {boolean} 'true' if the operation was done, 'false' if the connection no exists.
+ */
+jqSimpleConnect.showConnection = function (connectionId) {
+    if (typeof connectionId === 'object') {
+        $(connectionId).each(function () {
+            jqSimpleConnect.showConnection(this);
+        });
+        return;
+    }
+    if (jqSimpleConnect._connections[connectionId] != null) {
+        // Remove HTML element.
+        jQuery('.jqSimpleConnect.' + connectionId).show();
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Removes a connection.
+ *
+ * @param {string} connectionId The connection identifier.
+ * @returns {boolean} 'true' if the operation was done, 'false' if the connection no exists.
+ */
+jqSimpleConnect.showConnections = function (connectionId) {
+    for (var connId in connectionId) {
+        jqSimpleConnect.showConnection(connectionId[connId]);
+    }
+}
+
 /**
  * Removes all the connections.
  */
-jqSimpleConnect.removeAll = function() {
+jqSimpleConnect.removeAll = function () {
     // Remove HTML elements.
     jQuery('.jqSimpleConnect').remove();
-    
+
     // Clear connections list.
-    for(var key in jqSimpleConnect._connections) {
+    for (var key in jqSimpleConnect._connections) {
         jqSimpleConnect._connections[key] = null;
         delete jqSimpleConnect._connections[key];
     }
 }
+
+/**
+ * Added listener to window resize event.
+ */
+jQuery(window).resize(function(){
+    jqSimpleConnect.repaintAll();
+});
